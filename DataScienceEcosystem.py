272f7b45015b947611dac94e117f21e2439d73eb
niyasbinsahad007 <147399216+niyasbinsahad007@users.noreply.github.com>
2023-10-16 14:08:45 +0300

    notebook

---
+++ DataScienceEcosystem.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Data Science Tools and Ecosystem
+
+# In[9]:
+
+
+print ("in this notebook provide informations of overview of data science tool, languages of data science, libraries,APIs,Datasets,jupyter project,Rstudios,GitHub ")
+
+
+# ## Some of the popular languages that Data Scientists
+
+# In[13]:
+
+
+print ("1.phython 2.R 3.scala")
+
+
+# ##  Data science libraries
+
+# In[14]:
+
+
+print("pandas,matplotlib,scikit-learn,TensorFlow")
+
+
+# ## Data Science Tools
+
+# |             Data Science Tools        |
+# 
+
+# ### Below are a few examples of evaluating arithmetic expressions in Python
+
+# In[15]:
+
+
+print("This a simple arithmetic expression to mutiply then add integers")
+
+
+# In[18]:
+
+
+(3*4+5)
+
+
+# In[21]:
+
+
+200/60
+
+
+# ###### Objectives
+
+# <ul>
+#     <li>python</li>
+#     <li>R</li>
+#     <li>scala</li>
+#     <li>Julia</li>
+#     <li>java</li>
+#     
+#     
+#     
+
+# ## MUHAMMAD NIYAS KMK
